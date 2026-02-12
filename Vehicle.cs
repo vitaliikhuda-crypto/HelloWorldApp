95dbfcfb6b6abdaf48796ff4107d7ad8e6ab49b7
vitaliikhuda-crypto <vitalii.khuda@student.uzhnu.edu.ua>
2026-02-12 10:10:17 +0200

    Add Car, Bicycle, and Airplane classes with movement

--- Vehicle.cs
+++ Vehicle.cs
@@ -18,4 +18,56 @@ namespace Lab6_Vehicles
         public abstract void Move();
     }
 }
+using System;
+
+namespace Lab6_Vehicles
+{
+    class Car : Vehicle, IRefuelable
+    {
+        public Car(string brand, int speed) : base(brand, speed) { }
+
+        public override void Move()
+        {
+            Console.WriteLine($"Автомобіль {brand} рухається дорогою зі швидкістю {speed} км/год.");
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine($"Автомобіль {brand} заправляється пальним.");
+        }
+    }
+}
+using System;
+
+namespace Lab6_Vehicles
+{
+    class Bicycle : Vehicle
+    {
+        public Bicycle(string brand, int speed) : base(brand, speed) { }
+
+        public override void Move()
+        {
+            Console.WriteLine($"Велосипед {brand} рухається зі швидкістю {speed} км/год.");
+        }
+    }
+}
+using System;
+
+namespace Lab6_Vehicles
+{
+    class Airplane : Vehicle, IRefuelable
+    {
+        public Airplane(string brand, int speed) : base(brand, speed) { }
+
+        public override void Move()
+        {
+            Console.WriteLine($"Літак {brand} летить зі швидкістю {speed} км/год.");
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine($"Літак {brand} заправляється авіаційним паливом.");
+        }
+    }
+}
 
